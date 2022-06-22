6ed25c91e730217e2745bf007c57a0d5286875c2
Daniel Chimento <danchimento@live.com>
2022-06-22 15:29:42 -0400

    Adding bitcoint

--- ExchangeRateController.cs
+++ ExchangeRateController.cs
@@ -13,10 +13,36 @@ namespace api.Controllers
     public class ExchangeRateController : ControllerBase
     {
         private readonly ILogger<ExchangeRateController> _logger;
+        private readonly HttpClient _httpClient;
 
         public ExchangeRateController(ILogger<ExchangeRateController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
+        }
+
+        /// <summary>
+        /// Fetches rates
+        /// </summary>
+        /// <note>
+        /// Exceptions will bubble up to the client.
+        /// No error handling required because no exception is expected.
+        /// </note>
+        /// <returns>
+        /// A collection or exchange rates
+        /// </returns>
+        [HttpGet]
+        public async Task<IEnumerable<ExchangeRate>> Get()
+        {
+            var streamTask = _httpClient.GetStreamAsync("https://api.coindesk.com/v1/bpi/currentprice.json");
+            var rates = await JsonSerializer.DeserializeAsync<ExchangeRatesResponse>(await streamTask);
+
+            // The result from the API are in the format:
+            // { 'USD': { code: 'USD', rate: 1.00 }}
+            // We don't need the key here because the value contains the currency code
+            var results = rates.bpi.Select(r => r.Value);
+
+            return results;
         }
     }
 }
