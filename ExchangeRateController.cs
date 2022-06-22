fef911e7ffd4ebd22860cfb59d90d8b722417215
Daniel Chimento <danchimento@live.com>
2022-06-22 15:25:39 -0400

    Moving controller

---
+++ ExchangeRateController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExchangeRateController : ControllerBase
+    {
+        private readonly ILogger<ExchangeRateController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public ExchangeRateController(ILogger<ExchangeRateController> logger)
+        {
+            _logger = logger;
+            _httpClient = new HttpClient();
+        }
+    }
+}
